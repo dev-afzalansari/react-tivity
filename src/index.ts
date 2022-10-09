91edabb803ab1b93f4737c0363937ed02d233c63
afzal ansari <dev.afzalansari@gmail.com>
2022-10-09 17:26:35 +0545

    deprecate internal api

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { default as create } from "./apis/create";
 export { default as persist } from "./apis/persist";
 export { default as reduce } from "./apis/reduce";
-export { default as hook } from "./apis/hook";
