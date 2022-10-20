fd3bf39b14f4f8a8fc4e88fff312a0fbbd765186
Afzal Ansari <dev.afzalansari@gmail.com>
2022-10-20 12:50:03 +0000

    add proxy middleware

---
+++ src/__tests__/with-proxy.tsx
@@ -0,0 +1,99 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+
+import { create } from '..'
+import { proxy } from '../proxy'
+import type { StateCopy, StateObj } from '../inits/initStore'
+
+describe('tests with create', () => {
+
+    let initObj = (): StateObj => ({
+        count: 0,
+        inc: (state: StateCopy) => state.count++,
+        dec: (state: StateCopy) => state.count--,
+        title: "nothing",
+        setTitle: (state: StateCopy, newTitle: string) => {
+          state.title = newTitle
+        },
+      })
+    
+      let useStore: any
+    
+      beforeEach(() => {
+        useStore = create(proxy(initObj()))
+      })
+    
+      afterEach(() => {
+        useStore = null
+      })
+
+    test('returns the selected state', async () => {
+      function Component() {
+        let count = useStore('count')
+        let title = useStore((state: StateObj) => state.title)
+
+        return (
+          <div>
+            <h1>{count}</h1>
+            <h1>{title}</h1>
+          </div>
+        )
+      }
+
+      let { findByText } = render(<Component />)
+
+      await findByText('0')
+      await findByText('nothing')
+    })
+
+    test('updates the state when state object is mutated', async () => {
+
+      function Component() {
+        let { count, title, inc, dec, setTitle } = useStore()
+
+        return (
+          <div>
+            <h1>{count}</h1>
+            <h1>{title}</h1>
+            <button onClick={inc}>inc</button>
+            <button onClick={dec}>dec</button>
+            <button onClick={() => setTitle('something')}>change</button>
+          </div>
+        )
+      }
+
+      let { findByText, getByText } = render(<Component />)
+
+      await findByText('0')
+      await findByText('nothing')
+
+      fireEvent.click(getByText('inc'))
+      fireEvent.click(getByText('change'))
+
+      await findByText('1')
+      await findByText('something')
+
+      fireEvent.click(getByText('dec'))
+      fireEvent.click(getByText('dec'))
+
+      await findByText('-1')
+    })
+
+    test('with store apis', async () => {
+
+      expect(useStore.state.get('count')).toBe(0)
+      expect(useStore.state.get('title')).toBe('nothing')
+
+      useStore.state.inc()
+      useStore.state.setTitle('something')
+
+      expect(useStore.state.get('count')).toBe(1)
+      expect(useStore.state.get('title')).toBe('something')
+
+      useStore.state.dec()
+      useStore.state.dec()
+
+      expect(useStore.state.get('count')).toBe(-1)
+
+    })
+})
---
+++ src/proxy.ts
@@ -0,0 +1,35 @@
+import type { StateCopy, StateObj } from './inits/initStore'
+
+export function proxy(initializer: any, isMethod = false) {
+  const handler = {
+    set: (state: StateCopy, prop: string, value: any) => {
+      let obj: any = {}
+      obj[prop] = value
+      state.set(obj)
+      return true
+    },
+    get: (state: StateCopy, prop: string) => {
+      return state.get(prop)
+    }
+  }
+
+  const customMethod = (state: StateCopy, ...args: any[]) => {
+    let prx = new Proxy(state, handler)
+    typeof initializer === 'function' ? initializer(prx, ...args) : null
+    return undefined
+  }
+
+  if (isMethod) {
+    return customMethod
+  }
+  let initObj = typeof initializer === 'function' ? initializer() : initializer
+  let state: StateObj = {}
+  Object.keys(initObj).forEach(key => {
+    if (typeof initObj[key] === 'function') {
+      state[key] = proxy(initObj[key], true)
+    } else {
+      state[key] = initObj[key]
+    }
+  })
+  return state
+}
